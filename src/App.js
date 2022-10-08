9d80dee574921a49efaca6454470ab0c13f0c956
scain5878 <scain5878@gmail.com>
2022-10-08 14:51:32 +0530

    created home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import MarkdownArea from "./components/markdownArea/MarkdownArea";
 import Navbar from "./contianer/header/Navbar";
+import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
+import Home from "./contianer/home/Home";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <MarkdownArea/>
+      <Router>
+        <Routes>
+          <Route index element={<Home/>}/>
+          <Route path="/markdown-editor" element={<MarkdownArea/>}/>
+        </Routes>
+      </Router>
+
+
+      {/* <Navbar/>
+      <MarkdownArea/> */}
     </div>
   );
 }
