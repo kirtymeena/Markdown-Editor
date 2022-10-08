b5f157b6a8d8d3fd25cfbba582bfb7f7e44f37dd
scain5878 <scain5878@gmail.com>
2022-10-08 10:51:12 +0530

    User can change file name

--- src/contianer/header/Navbar.js
+++ src/contianer/header/Navbar.js
@@ -3,26 +3,29 @@ import { Inplace, InplaceDisplay, InplaceContent } from 'primereact/inplace';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteFile, updateSave } from '../../redux/features/saveSlice';
+import { deleteFile, updatefileName, updateSave } from '../../redux/features/saveSlice';
 import { deleteText } from '../../redux/features/textSlice';
 
 function Navbar() {
-    const [documentName, setDocumentName] = useState('welcome')
+    const [documentName, setDocumentName] = useState('')
     const dispatch = useDispatch()
-    const sState = useSelector(state => state)
+    const state = useSelector(state => state)
 
     const changeSaveState = () => {
         dispatch(updateSave())
     }
     const HandleDelete = () => {
         dispatch(deleteFile())
-        dispatch(deleteText({text:''}))
-        
+        dispatch(deleteText({ text: undefined }))
+
+    }
+    const setFileName = (e) => {
+        dispatch(updatefileName({ fileName: e.target.value }))
     }
 
-    useEffect(()=>{
+    useEffect(() => {
 
-    },[sState.save])
+    }, [state.save, state.saveState.fileName.fileName])
 
     return (
         <>
@@ -33,11 +36,11 @@ function Navbar() {
                     <div>
                         <Inplace closable>
                             <InplaceDisplay>
-                                <p><span style={{ padding: "0.7rem" }}> <i className="pi pi-file-edit" style={{ fontSize: "1.2rem" }}></i></span>{documentName}</p>
+                                <p><span style={{ padding: "0.7rem" }}> <i className="pi pi-file-edit" style={{ fontSize: "1.2rem" }}></i></span>{state.saveState.fileName.fileName || "welcome"}</p>
                             </InplaceDisplay>
                             <InplaceContent>
 
-                                <InputText autoFocus value={documentName} onChange={(e) => setDocumentName(e.target.value)} className="p-inputtext-sm block mb-2" />
+                                <InputText autoFocus value={state.saveState.fileName.fileName} onChange={(e) => setFileName(e)} className="p-inputtext-sm block mb-2" />
                             </InplaceContent>
                         </Inplace>
                     </div>
@@ -66,9 +69,10 @@ function Navbar() {
                     <div className='file__name'>
                         <Inplace closable >
                             <InplaceDisplay>
-                                <p><span style={{ padding: "0.7rem" }}> <i className="pi pi-file-edit" style={{ fontSize: "1.2rem" }}></i></span>{documentName}</p>                            </InplaceDisplay>
+                                <p><span style={{ padding: "0.7rem" }}> <i className="pi pi-file-edit" style={{ fontSize: "1.2rem" }}></i></span>{state.saveState.fileName.fileName || "welcome"}</p>
+                            </InplaceDisplay>
                             <InplaceContent>
-                                <InputText autoFocus value={documentName} onChange={(e) => setDocumentName(e.target.value)} />
+                                <InputText autoFocus value={state.saveState.fileName.fileName} onChange={(e) => setFileName(e)} />
                             </InplaceContent>
                         </Inplace>
                     </div>
